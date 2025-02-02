f359e3a53e05a00d629c2a34367b22df25c839fb
Marcin <contact@mosk.it>
2025-02-02 20:31:29 +0100

    Disable evil-snipe-mode

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -74,3 +74,7 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+;; CUSTOM
+
+;; remove evil-snipe-mode
+(remove-hook 'doom-first-input-hook #'evil-snipe-mode)
